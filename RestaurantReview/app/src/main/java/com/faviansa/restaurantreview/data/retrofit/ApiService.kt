a92274ecfe50d309425235f2995e513cae8811a4
BrianFaviansa <brianfpd31@gmail.com>
2024-09-24 12:51:19 +0700

    feat: fetch data from API

---
+++ RestaurantReview/app/src/main/java/com/faviansa/restaurantreview/data/retrofit/ApiService.kt
@@ -0,0 +1,13 @@
+package com.faviansa.restaurantreview.data.retrofit
+
+import com.faviansa.restaurantreview.data.response.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ) : Call<RestaurantResponse>
+}
